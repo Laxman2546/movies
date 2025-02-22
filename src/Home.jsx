898990a6591fc96bd911dda0836954882d93541f
ELLA LAKSHMAN <laxmannani960@gmail.com>
2025-02-22 16:37:11 +0530

    changed

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import Navbar from "./navbar";
+import Navbar from "./Navbar";
 import Credits from "./credits";
 import noposter from "./assets/noposter.jpg";
 const Home = () => {
--- src/Movieplayer.jsx
+++ src/Movieplayer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import Moviedetails from "./movieDetails";
-import Navbar from "./navbar";
+import Navbar from "./Navbar";
 const Movieplayer = () => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
