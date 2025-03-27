15b2cf6f5b274746025aa0086a8276859450a0e0
ELLA LAKSHMAN <laxmannani960@gmail.com>
2025-03-27 17:34:46 +0530

    updated config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/", 
+  base: "/", // Ensures Vite generates correct paths
   build: {
-    outDir: "dist",
+    outDir: "dist", // Ensure output is in dist/
   },
   server: {
     host: true,
